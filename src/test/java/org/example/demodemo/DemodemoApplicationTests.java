2662e4ce75fd1b0a7948a935f88b088720bc0250
600gramSik <geunsikevin@naver.com>
2024-03-29 17:41:41 +0900

    1

---
+++ src/test/java/org/example/demodemo/DemodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
